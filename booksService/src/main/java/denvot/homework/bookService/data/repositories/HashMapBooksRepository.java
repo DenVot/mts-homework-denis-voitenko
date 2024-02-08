0f66aee4e218adc2f3d027b113620809e42a2e80
Denis Voitenko <denis@denvot.dev>
2024-02-08 19:20:37 +0300

    Получение книг по тегу

--- booksService/src/main/java/denvot/homework/bookService/data/repositories/HashMapBooksRepository.java
+++ booksService/src/main/java/denvot/homework/bookService/data/repositories/HashMapBooksRepository.java
@@ -4,7 +4,9 @@ import denvot.homework.bookService.data.entities.Book;
 import denvot.homework.bookService.data.entities.BookId;
 import denvot.homework.bookService.data.repositories.exceptions.BookNotFoundException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,6 +18,7 @@ public class HashMapBooksRepository implements BooksRepository {
     this.books = books;
   }
 
+  @Override
   public Book createBook(String author, String title, Set<String> tags) {
     var bookId = new BookId(nextBookId.getAndIncrement());
     var book = new Book(bookId, author, title, tags);
@@ -24,14 +27,32 @@ public class HashMapBooksRepository implements BooksRepository {
     return book;
   }
 
+  @Override
   public Book findBook(BookId id) throws BookNotFoundException {
     failIfBookNotExists(id);
     return books.get(id);
   }
 
+  @Override
   public void deleteBook(BookId id) throws BookNotFoundException {
     failIfBookNotExists(id);
     books.remove(id);
+  }
+
+  @Override
+  public ArrayList<Book> getByTags(HashSet<String> tags) {
+    var list = new ArrayList<Book>();
+
+    for (var book : books.values()) {
+      var tempTags = new HashSet<>(tags);
+      tempTags.retainAll(book.getTags());
+
+      if (!tempTags.isEmpty()) {
+        list.add(book);
+      }
+    }
+
+    return list;
   }
 
   private void failIfBookNotExists(BookId id) throws BookNotFoundException {
