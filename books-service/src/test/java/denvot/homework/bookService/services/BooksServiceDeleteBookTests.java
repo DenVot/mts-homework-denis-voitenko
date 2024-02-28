3fe4ddb738ce40ababe3066e9407fb5326aa9164
Denis Voitenko <denis@denvot.dev>
2024-02-28 19:17:27 +0300

    Удаление книги (адаптация)

--- books-service/src/test/java/denvot/homework/bookService/services/BooksServiceDeleteBookTests.java
+++ books-service/src/test/java/denvot/homework/bookService/services/BooksServiceDeleteBookTests.java
@@ -1,39 +1,66 @@
 package denvot.homework.bookService.services;
 
 
-import denvot.homework.bookService.data.repositories.BooksRepositoryBase;
+import denvot.homework.bookService.DatabaseSuite;
+import denvot.homework.bookService.data.entities.Author;
+import denvot.homework.bookService.data.entities.Book;
+import denvot.homework.bookService.data.repositories.DbBooksRepository;
 import denvot.homework.bookService.data.repositories.exceptions.BookNotFoundException;
+import denvot.homework.bookService.data.repositories.jpa.JpaAuthorsRepository;
+import denvot.homework.bookService.data.repositories.jpa.JpaBooksRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class BooksServiceDeleteBookTests {
-  private BooksRepositoryBase repository;
+@DataJpaTest
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+@Import({BooksService.class, DbBooksRepository.class})
+public class BooksServiceDeleteBookTests extends DatabaseSuite {
+  @Autowired
+  private JpaBooksRepository booksRepository;
+
+  @Autowired
+  private JpaAuthorsRepository authorsRepository;
+
+  @Autowired
   private BooksService booksService;
+
+  private Book testBook;
 
   @BeforeEach
   public void setUp() {
-    repository = mock(BooksRepositoryBase.class);
-    booksService = new BooksService(repository);
+    booksRepository.deleteAll();
+    authorsRepository.deleteAll();
+
+    var testAuthor = new Author("Test", "Author");
+    authorsRepository.save(testAuthor);
+
+    testBook = new Book("Test Book", testAuthor);
+    booksRepository.save(testBook);
   }
 
   @Test
-  public void testSimpleDelete() throws BookNotFoundException {
-    boolean isDeleted = booksService.deleteBook(1L);
-
-    verify(repository).deleteBook(1L);
-    Assertions.assertTrue(isDeleted);
+  public void testSimpleDelete() {
+    assertTrue(booksService.deleteBook(testBook.getId()));
+    assertEquals(0, booksRepository.findAll().size());
   }
 
   @Test
   public void testDeleteBookNotExists() throws BookNotFoundException {
-    doThrow(new BookNotFoundException()).when(repository).deleteBook(1L);
+    boolean isDeleted = booksService.deleteBook(testBook.getId() + 1);
 
-    boolean isDeleted = booksService.deleteBook(1L);
-
-    verify(repository).deleteBook(1L);
     Assertions.assertFalse(isDeleted);
   }
 }
