61dacd7add950ecdda246f6b18c4ac820d773516
Denis Voitenko <denis@denvot.dev>
2024-02-29 10:42:01 +0300

    ddl-auto create

--- books-service/src/test/java/denvot/homework/bookService/DatabaseSuite.java
+++ books-service/src/test/java/denvot/homework/bookService/DatabaseSuite.java
@@ -20,8 +20,8 @@ public class DatabaseSuite {
       TestPropertyValues.of(
               "spring.datasource.url=" + POSTGRES.getJdbcUrl(),
               "spring.datasource.username=" + POSTGRES.getUsername(),
-              "spring.datasource.password=" + POSTGRES.getPassword()
-              //"spring.jpa.hibernate.ddl-auto=create"
+              "spring.datasource.password=" + POSTGRES.getPassword(),
+              "spring.jpa.hibernate.ddl-auto=create"
       ).applyTo(context);
     }
   }
