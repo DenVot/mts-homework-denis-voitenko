1fb8b07d98890c113c62e461f1408f4f03abaa6c
Denis Voitenko <denis@denvot.dev>
2024-02-29 12:43:46 +0300

    Создание тега

---
+++ books-service/src/main/java/denvot/homework/bookService/services/TagsService.java
@@ -0,0 +1,27 @@
+package denvot.homework.bookService.services;
+
+import denvot.homework.bookService.data.entities.Tag;
+import denvot.homework.bookService.data.repositories.jpa.JpaTagsRepository;
+import denvot.homework.bookService.exceptions.TagAlreadyExistsException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class TagsService {
+
+  private final JpaTagsRepository jpaTagsRepository;
+
+  public TagsService(JpaTagsRepository jpaTagsRepository) {
+    this.jpaTagsRepository = jpaTagsRepository;
+  }
+
+  @Transactional(propagation = Propagation.REQUIRED)
+  public Tag createNew(String tagName) throws TagAlreadyExistsException {
+    if (jpaTagsRepository.findByName(tagName).isPresent()) {
+      throw new TagAlreadyExistsException();
+    }
+
+    return jpaTagsRepository.save(new Tag(tagName));
+  }
+}
