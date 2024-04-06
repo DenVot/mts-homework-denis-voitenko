c3b925bf419d7778527ac492f7106b2be65c40dc
Denis Voitenko <denis@denvot.dev>
2024-04-06 16:48:40 +0300

    Initial

---
+++ book-purchase-service/src/main/java/denvot/homework/bookpurchaseservice/BookPurchaseServiceApplication.java
@@ -0,0 +1,13 @@
+package denvot.homework.bookpurchaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPurchaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPurchaseServiceApplication.class, args);
+	}
+
+}
---
+++ book-purchase-service/src/test/java/denvot/homework/bookpurchaseservice/BookPurchaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package denvot.homework.bookpurchaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPurchaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
