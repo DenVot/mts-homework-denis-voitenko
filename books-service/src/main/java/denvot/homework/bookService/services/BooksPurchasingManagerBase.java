c7354ef74085fa02d8a1d795498ab43ef0c4ec3d
Denis Voitenko <denis@denvot.dev>
2024-04-07 00:57:08 +0300

    Purchasing

--- books-service/src/main/java/denvot/homework/bookService/services/BooksPurchasingManagerBase.java
+++ books-service/src/main/java/denvot/homework/bookService/services/BooksPurchasingManagerBase.java
@@ -1,7 +1,8 @@
 package denvot.homework.bookService.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import denvot.homework.bookService.data.repositories.exceptions.BookNotFoundException;
 
 public interface BooksPurchasingManagerBase {
-  void createPurchasing(Long id) throws JsonProcessingException;
+  void createPurchasing(Long id) throws JsonProcessingException, BookNotFoundException;
 }
--- books-service/src/test/java/denvot/homework/bookService/services/books/BooksPurchasingManagerTests.java
+++ books-service/src/test/java/denvot/homework/bookService/services/books/BooksPurchasingManagerTests.java
@@ -1,11 +1,17 @@
 package denvot.homework.bookService.services.books;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import denvot.homework.bookService.DatabaseSuite;
+import denvot.homework.bookService.data.entities.Author;
+import denvot.homework.bookService.data.entities.Book;
+import denvot.homework.bookService.data.repositories.exceptions.BookNotFoundException;
+import denvot.homework.bookService.data.repositories.jpa.JpaBooksRepository;
 import denvot.homework.bookService.outbox.OutboxSchedulerBase;
 import denvot.homework.bookService.services.BooksPurchasingManager;
 import org.junit.jupiter.api.Test;
@@ -19,6 +25,8 @@ import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
+
+import java.util.Optional;
 
 @SpringBootTest(classes = {BooksPurchasingManager.class})
 @Import({BooksPurchasingManagerTests.ObjectMapperTestConfig.class})
@@ -38,11 +46,15 @@ public class BooksPurchasingManagerTests extends DatabaseSuite {
   @MockBean
   private OutboxSchedulerBase outboxScheduler;
 
+  @MockBean
+  private JpaBooksRepository booksRepository;
+
   @Autowired
   private BooksPurchasingManager booksPurchasingManager;
 
   @Test
-  public void testSend() throws JsonProcessingException {
+  public void testSend() throws JsonProcessingException, BookNotFoundException {
+    when(booksRepository.findById(anyLong())).thenReturn(Optional.of(new Book("Test", new Author("Test", "Author"))));
     booksPurchasingManager.createPurchasing(0L);
     verify(outboxScheduler).scheduleMsg(any());
   }
